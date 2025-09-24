832ac116d28cc88333fde324c0efc0f57b8651b3
FelipeA <felipe.aguero08@inacapmail.cl>
2025-09-23 22:27:44 -0300

    AAHHLISTOO

--- Biblioteca_project/urls.py
+++ Biblioteca_project/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include 
 from rest_framework import routers 
 from . import views 
@@ -6,11 +7,11 @@ router = routers.DefaultRouter()  # este elemento enrutador permite manejar múl
 router.register(r'comuna', views.ComunaViewSet)
 router.register(r'nacionalidad', views.NacionalidadViewSet)
 router.register(r'direccion', views.DireccionViewSet)
-router.register(r'autores', views.AutorViewSet)
+router.register(r'autor', views.AutorViewSet)
 router.register(r'biblioteca', views.BibliotecaViewSet)
 router.register(r'categoria', views.CategoriaViewSet)
-router.register(r'libros', views.LibroViewSet)
-router.register(r'lectores', views.LectorViewSet)
+router.register(r'libro', views.LibroViewSet)
+router.register(r'lector', views.LectorViewSet)
 router.register(r'prestamo', views.PrestamoViewSet)
 
 urlpatterns = [
--- drf/urls.py
+++ drf/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for drf project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from Biblioteca_project.views import InicioPaginaView, MenuPrincipalView, AutoresView, LectoresView, LibrosView
@@ -22,8 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls), 
     path('', InicioPaginaView.as_view(), name='inicio'), # la ruta raíz que apunta a la vista de inicio
     path('menu/', MenuPrincipalView.as_view(), name='menu'), #ruta para el menú principal
-    path('libros/', LibrosView.as_view(), name='libros'),
-    path('autores/', AutoresView.as_view(), name='autores'),
-    path('lectores/', LectoresView.as_view(), name='lectores'),
-    path('api/', include('Biblioteca_project.urls')),
+    path('Biblioteca_project/libros/', LibrosView.as_view(), name='libros'),
+    path('Biblioteca_project/autores/', AutoresView.as_view(), name='autores'),
+    path('Biblioteca_project/lectores/', LectoresView.as_view(), name='lectores'),
+    path('Biblioteca_project/', include('Biblioteca_project.urls')),
 ]
